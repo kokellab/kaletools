7707417448f071cd4f2d7eae58fb1cc8c7c87439
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2016-11-18 10:36:16 -0800

    Fixed error in template.

--- bash/template.sh
+++ bash/template.sh
@@ -6,11 +6,11 @@ IFS=$'\n\t'
 if (( $# == 1 )) && [[ "${1}" == "--help" ]]; then
 	echo "Usage: ${0} [--some-flag] [--some-optional-arg value] <arg1> <arg2> [optional-arg]"
 	echo "A description of what this does"
-	return 0
+	exit 0
 fi
 
 # validate the arguments; this example requires between 2 and 6 arguments
 if (( $# < 2 )) || (( $# > 6 )); then
 	(>&2 	echo "Usage: ${0} [--some-flag] [--some-optional-arg value] <arg1> <arg2> [optional-arg]")
-	return 1
+	exit 1
 fi
