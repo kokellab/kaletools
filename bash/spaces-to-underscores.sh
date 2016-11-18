5c3fa4563919b18c0d3b03fed65a3375c223f972
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2016-11-18 10:35:22 -0800

    Fixed errors in spaces-to-underscores.sh.

--- bash/spaces-to-underscores.sh
+++ bash/spaces-to-underscores.sh
@@ -3,15 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if (( $# == 1 )) && [[ "${1}" == "--help" ]]; then
-	echo "Usage: ${0} <path>"
+	echo "Usage: ${0} [-h/--help] <path>"
 	echo "Replaces the spaces with underscores in every filename in <path> or its subdirectories (recursively)"
-	return 0
+	exit 0
 fi
 
 if (( $# != 1 )); then
-	(>&2 echo "Usage: ${0} <path>")
-	return 1
+	(>&2 echo "Usage: [-h/--help] ${0} <path>")
+	exit 1
 fi
 
 find "${1}" -depth -name '* *' \
 | while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
+
