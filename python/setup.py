de8602256b40cdefe6dac00818844887b35f03b4
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2016-12-12 17:46:53 -0800

    Moved Python scripts to kltools package.

---
+++ python/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+	name='kltools',
+	version='0.1',
+	description='Command-line Python tools for the Kokel Lab',
+	author='Douglas Myers-Turnbull',
+	url='https://github.com/kokellab/kltools',
+	packages=['kltools'],
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		'Intended Audience :: Science/Research',
+		'Natural Language :: English'
+		'Operating System :: POSIX',
+		'Programming Language :: Python :: 3 :: Only',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Scientific/Engineering :: Bio-Informatics'
+	],
+)
