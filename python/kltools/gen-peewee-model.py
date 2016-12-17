0dc4b3ca1a51fdce822612a8d075fcbe288173e4
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2016-12-16 22:08:33 -0800

    Don't hardcode lines to replace, and allow a different port.

--- python/kltools/gen-peewee-model.py
+++ python/kltools/gen-peewee-model.py
@@ -20,19 +20,21 @@ def _lines(filename: str) -> Iterator[str]:
 	with open(filename, 'r') as f:
 		for line in f: yield line.rstrip('\n\r')
 
-def gen_model(host: str, username: str, db: str, output: str) -> None:
+def gen_model(host: str, username: str, db: str, port: int, output: str) -> None:
 	with open(output, 'w') as f:
-		subprocess.call([sys.executable, '-m', 'pwiz', '-e', 'mysql', '-H', host, '-u', username, '-P', db], stdout=f)
+		subprocess.call([sys.executable, '-m', 'pwiz', '-e', 'mysql', '-H', host, '-u', username, '-p', port, '-P', db], stdout=f)
 
-def fix_connection(output: str) -> None:
+def fix_connection(output: str, header_file: str) -> None:
+
+	header_lines = ''
+	if header_file is not None:
+		with open(header_file, 'r') as f:
+			header_lines = f.read()
+
 	for line in fileinput.input(output, inplace=True):
 		if fileinput.filelineno() == 1:
-			print("from peewee import *")
-		elif fileinput.filelineno() == 2:
-			print("from .db import config")
-		elif fileinput.filelineno() == 3:
-			print("database = MySQLDatabase(config['db'], **{'user': config['user'], 'password': config['password'], 'host': config['host'], 'port': config['port']})")
-		else:
+			print(header_lines)
+		elif fileinput.filelineno() > 3:
 			print(line.rstrip())
 
 def find_blob_columns(schema: str) -> Set[str]:
@@ -47,23 +49,25 @@ def fix_blobs(columns: Set[str], output: str):
 		else:
 			print(line.rstrip())
 
-def run(host: str, username: str,  db: str, schema: str, output: str) -> None:
-	gen_model(host, username, db, output)
-	fix_connection(output)
+def run(host: str, username: str, db: str, port: int, header_file: str, schema: str, output: str) -> None:
+	gen_model(host, username, db, port, output)
+	fix_connection(output, header_file)
 	blob_columns = find_blob_columns(schema)
 	logging.info("Fixing columns {}".format(blob_columns))
 	fix_blobs(blob_columns, output)
 
 def main(opts: List[str]) -> None:
-	run(opts.host, opts.username, opts.db, opts.schema, opts.output)
+	run(opts.host, opts.username, opts.db, opts.port, opts.header_file, opts.schema, opts.output)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Generates a peewee model and fixes the connection info and binary/blob columns.')
 	parser.add_argument('--output', required=True, type=str, help='Output model.py file')
-	parser.add_argument('--host', required=True, type=str, help='The database connection server')
+	parser.add_argument('--host', default='localhost', type=str, help='The database connection server')
 	parser.add_argument('--username', required=True, type=str, help='The database username')
 	parser.add_argument('--db', required=True, type=str, help='The name of the database')
+	parser.add_argument('--port', default=3306, type=str, help='The port number')
 	parser.add_argument('--schema', required=True, type=str, help='The path to the schema .sql file; must be for this database ONLY')
+	parser.add_argument('--header-file', required=False, type=str, help='File containing header lines to replace the first 3 lines of the model file with')
 	opts = parser.parse_args()
 	main(opts)
 
