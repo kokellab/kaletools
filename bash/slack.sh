1a05ee330dd9d1faabd6db288b1be9395e0e9286
David Kokel <dkokel@users.noreply.github.com>
2017-12-18 17:44:56 -0800

    Slack channel

--- bash/slack.sh
+++ bash/slack.sh
@@ -1,24 +1,27 @@
-# Author: Douglas
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
 
-if (( $# != 3 )); then
-	(>&2 echo "Usage: ${0} <webhook_url> <channel> <message>")
-	exit 1
-fi
-webhook_url=$1
-channel=$2
-text=$3
+date_time=$(date '+%Y-%m-%d')
+bup_path=/Volumes/Drobo5D2_Valinor_backup
+log_path=/Users/davekokel/valinor-backup-logs/${date_time}.log
+echo "Backing up to ${bup_path}"
+echo "Logging to ${log_path}"
+display_host="Gondor"
 
-if [[ $text == "" ]]; then
-        echo "No text specified"
-        exit 1
-fi
+bup () {
+	echo "Copying $1 to $bup_path/$2"
+	rsync --update --recursive --times --no-links --progress --log-file=$log_path valinor:$1 $bup_path/$2
+	echo "==========FINISHED COPYING ${1} at $(date '+%Y-%m-%d')==========" >> $log_path
+	$HOME/bin/slack.sh "${slack_url}" "notifications" "${display_host} backed up ${1} from Valinor"
+}
 
-escaped=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
+bup /alpha/backups ""
+bup /alpha/plates ""
+bup /alpha/legacy ""
+bup /alpha/orphaned ""
+bup /alpha/logs/submissions "logs/"
 
-json="{\"link_names\":1,\"channel\": \"$channel\", \"attachments\":[{\"text\": \"$escaped\"}]}"
+echo "==========COMPLETED BACKUP at $(date '+%Y-%m-%d')==========" >> $log_path
+$HOME/bin/valinor-to-slack "${display_host} completed series of Valinor backups"
 
-curl -s -d "payload=$json" "$webhook_url"
+gzip ${log_path}
 
